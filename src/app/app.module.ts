f2750a827a1a0cd8bd440710a1a74a5a88c3133b
Rubén Garabaya <rubengarabaya@gmail.com>
2020-04-01 18:19:02 +0200

    enrutador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 import { LibrosComponent } from './libros/libros.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+
+const rutas: Routes = [
+  {path: 'listado-libros', component: LibrosComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { CabeceraComponent } from './cabecera/cabecera.component';
     CabeceraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
