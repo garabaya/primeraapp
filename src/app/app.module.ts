9a21f7a083de2bdbd092466073dd8d420df32073
Rubén Garabaya <rubengarabaya@gmail.com>
2020-04-01 18:10:44 +0200

    sobre-nosotros libros cabecera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { LibrosComponent } from './libros/libros.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreNosotrosComponent,
+    LibrosComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule
